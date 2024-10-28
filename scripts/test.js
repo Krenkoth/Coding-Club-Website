99805afd3682a123e2b56653552b7fa411abe50e
Krenkoth <john.g.skelton21@gmail.com>
2024-10-28 10:06:43 -0700

    JavaScript Practice

--- scripts/test.js
+++ scripts/test.js
@@ -1,8 +1,22 @@
 const buttonA = document.querySelector("#button_A");
-const hA = document.querySelector("#heading_A");
+const headingA = document.querySelector("#heading_A");
+const headingB = document.querySelector("#heading_B");
 
-let count = 1;
+var count = 0;
+var start = 0;
 
-function changeColor() {
-    hA.color = "blue";
-}
+
+buttonA.onclick = () => {
+    if (start === 0) {
+        start = Date();
+    }
+    count += 1;
+    let elapsed = (Date() - start) / 1000;
+    if (elapsed > 10) {
+        start = 0;
+        headingA.textContent = `CPS: ${elapsed}`;
+        count = 0;
+    }
+    headingB.textContent = `${Date() - Date()}`;
+    
+};
