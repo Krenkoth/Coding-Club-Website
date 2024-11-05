a7537b24b1c096df8370748bc611bce9f279869a
Krenkoth <john.g.skelton21@gmail.com>
2024-11-05 10:05:50 -0800

    Made hamburger nav

---
+++ scripts/top.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var links = document.getElementsByClassName("links")
+    if (links.style.display === "block") {
+      links.style.display = "none";
+    } else {
+      links.style.display = "block";
+    }
+  }
