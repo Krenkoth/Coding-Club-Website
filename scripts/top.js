b8d144d6b2cbc7ed72e51b9d2dee4ff10fbb23c7
Krenkoth <john.g.skelton21@gmail.com>
2024-11-06 09:50:57 -0800

    Hamburger kinda working?

--- scripts/top.js
+++ scripts/top.js
@@ -1,8 +1,28 @@
-function myFunction() {
-    var links = document.getElementsByClassName("links")
-    if (links.style.display === "block") {
-      links.style.display = "none";
-    } else {
+window.onload = function () {
+  var ham = document.querySelector(".icon");
+  ham.addEventListener('click', hamburg);  
+}
+
+
+
+
+function hamburg() {
+    var links = document.querySelector('.links');
+    // alert("display: " + links.style.display);
+    if (links.style.display ===  "none" || links.style.display === "") {
       links.style.display = "block";
+    } else {
+      links.style.display = "none";
     }
-  }
+    // alert("display: " + links.style.display)
+    var topBar = document.querySelector(".topBar");
+    // alert("height: " + topBar.style.height)
+    if (topBar.style.height === "6vh" || topBar.style.height === "") {
+      topBar.style.height = "18vh";
+    } else {
+      topBar.style.height = "6vh";
+    }
+    // alert("height: " + topBar.style.height);
+  
+}
+
