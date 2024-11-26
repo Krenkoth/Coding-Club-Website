058058c5f3fbec8d072e7c8b6c0bc539a9c9538f
Krenkoth <john.g.skelton21@gmail.com>
2024-11-26 09:18:36 -0800

    idk, some sturf

--- scripts/home.js
+++ scripts/home.js
@@ -1,25 +1,30 @@
 window.onload = function() {
-    const emailSubmit = document.querySelector(".emailSubmit");
-    emailSubmit.addEventListener("click", getEmail);
+    alert("monkey");
+    console.log("monkey");
+    // const emailSubmit = document.querySelector(".emailSubmit");
+    // emailSubmit.addEventListener("click", monkey);
 }
 
 
 
-
-
-
-function getEmail() {
-    const field = document.querySelector(".field");
-    const emailDisplay = document.querySelector("#email")
-    const userEmail = String(field.value);
-    if (isValidEmail(userEmail)) {
-        emailDisplay.textContent = `Email: ${userEmail}`;
-        field.value = "";
-    }
+// function getEmail() {
+//     const field = document.querySelector(".field");
+//     const emailDisplay = document.querySelector("#email");
+//     const userEmail = String(field.value);
+//     emailDisplay.textContent = `Email: ${userEmail}`;
+//     field.value = "";
+//     alert(emailDisplay.textContent);
+//     console.log("Finished");
+//     console.log(emailDisplay.textContent);
     
-}
+// }
 
-function isValidEmail(email) {
-    return email.includes('@');
-  }
+// function isValidEmail(email) {
+//     return email.includes('@');
+// }
+
+// function monkey() {
+//     alert("monkey");
+//     console.log("monkey");
+// }
 
