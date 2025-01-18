aa34b247080f6322e32281fe13739e7a85acdf0c
Krenkoth <john.g.skelton21@gmail.com>
2025-01-18 08:53:08 -0800

    Reduce redundancy & add style
    - Added creation of html elements for the top bar to the top.js script. Now there is one spot to change when changing nav
    - Used ChatGPT to make styles for some of the pages. I will get the rest tomorrow when ChatGPT lets me upload files again

--- scripts/top.js
+++ scripts/top.js
@@ -1,4 +1,8 @@
+addhtml();
 window.onload = function () {
+
+  
+
   var ham = document.querySelector(".icon");
   ham.addEventListener('click', hamburg);  
 }
@@ -24,5 +28,64 @@ function hamburg() {
     }
     // alert("height: " + topBar.style.height);
   
+}
+
+function addhtml() {
+  var file = document.currentScript.getAttribute("file");
+  var opener;
+  if (file === "index.html") {
+    opener = "";
+  } else {
+    opener = "../"
+  }
+
+  var container = document.createElement("div");
+
+  container.setAttribute("class", "topBar");
+  var header = document.createElement("header");
+  var link = document.createElement("a");
+  link.setAttribute("href", `${opener}index.html`);
+  link.setAttribute("id", "header-link");
+  var h1 = document.createElement("h1");
+  h1.textContent = "Coding Club";
+  link.appendChild(h1);
+  header.appendChild(link);
+  container.appendChild(header);
+
+  var nav = document.createElement("nav");
+  var ul = document.createElement("ul");
+  ul.setAttribute("class", "links");
+
+  makeNavLink("html/projects.html", "Projects", opener, ul);
+
+  makeNavLink("html/signin.html", "Sign In", opener, ul);
+
+  makeNavLink("html/about.html", "About", opener, ul);
+
+  nav.appendChild(ul);
+  var bars = document.createElement("a");
+  bars.setAttribute("href", "javascript:void(0);");
+  bars.setAttribute("class", "icon");
+  var i = document.createElement("i");
+  i.setAttribute("class", "fa fa-bars");
+  bars.appendChild(i);
+  nav.appendChild(bars);
+
+
+  container.appendChild(nav);
+
+  var body = document.querySelector("body");
+  body.appendChild(container);
+
+}
+
+function makeNavLink(href, name, opener, ul) {
+  var li = document.createElement("li");
+  var a = document.createElement("a");
+  a.setAttribute("href", `${opener}${href}`);
+  a.setAttribute("class", "top");
+  a.textContent = name;
+  li.appendChild(a);
+  ul.appendChild(li);
 }
 
